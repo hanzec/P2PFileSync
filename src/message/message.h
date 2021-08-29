0632fcbd22dd23e0685e6d3e4c10a3931437dfe7
Hanze Chen <tony.chen424@gmail.com>
2021-08-28 19:09:47 -0500

    code update

---
+++ src/message/message.h
@@ -0,0 +1,12 @@
+#ifndef P2P_FILE_SYNC_MESSAGE_MESSAGE_H
+#define P2P_FILE_SYNC_MESSAGE_MESSAGE_H
+
+#include "utils/ip_addr.h"
+
+namespace P2PFileSync {
+class Message {
+ public:
+  
+};
+}  // namespace P2PFileSync
+#endif
